b4f8e9fa005dd7826b46aa7514f22edafac5d160
AlejandroZBarajas <falejandrozb@gmail.com>
2024-08-01 19:59:45 -0600

    quickSort

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        QuickSort qs = new QuickSort();
+        int[] listaOG = new int[20];
+        int[] lista;
+        int sumaDer, sumaIzq;
+
+        qs.generarNumero(listaOG);
+        lista = listaOG.clone();
+        qs.ordenarQuickSort(lista, 0, lista.length - 1);
+
+        sumaIzq = qs.sumarParteIzq(lista, 0);
+        sumaDer = qs.sumarParteDer(lista, lista.length / 2);
+
+        if (sumaIzq > sumaDer) {
+            System.out.println("Lista ordenada:");
+            for (int i = 0; i < lista.length; i++) {
+                System.out.println(lista[i]);
+            }
+        }
+        
+    }
+    
+}
---
+++ QuickSort.java
@@ -0,0 +1,66 @@
+public class QuickSort {
+
+    public void generarNumero(int listaOG[]){
+        int numero;
+        System.out.println("Lista original");
+            for(int i=0; i<listaOG.length;i++){
+                numero = (int) (Math.random() * 50 +1);
+                listaOG[i] = numero;
+                System.out.println(listaOG[i]);
+            }
+    }
+
+    public void intercambiar(int lista[],int i, int j){
+        int aux = lista[i];
+        lista[i] = lista[j];
+        lista[j] = aux;
+    }
+
+    public void OrdenarQuickSort(int lista[], int inferior, int superior){
+        int i, j, central;
+        double pivote;
+        central = (inferior + superior)/2;
+        pivote = lista[central];
+
+        i=inferior;
+        j=superior;
+
+        System.out.println("Limites que se analizan: inferior ="+i+" superior="+j);
+        do {
+            while(lista[i] > pivote) i++;
+            while (lista[j] < pivote) j--;
+            if( i <= j){
+                intercambiar(lista,i,j);
+                i++;
+                j--;
+            }
+        } while (i <=j);
+        if (inferior < j)
+            OrdenarQuickSort(lista, inferior, j);
+        
+        if(i < superior)
+            OrdenarQuickSort(lista, i, superior);            }
+    }
+    
+
+    public void sumarParteIzq(int lista[], int indice){
+        int tam = indice;
+        int suma = 0;
+        if ( tam == 9){
+            suma = suma + lista [tam];
+        }else{
+            suma = lista[tam] + sumarParteIzq(lista, tam+1);
+        }return suma;
+    }
+
+    public void sumarParteDer(int lista [], int indice){
+        int tam = indice;
+        int suma = 0;
+
+        if (tam == lista.length-1){
+            suma = suma + lista[tam];
+        }
+    }
+
+
+}
